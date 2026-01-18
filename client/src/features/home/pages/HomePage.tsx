389bf71cfddacd4e00d31f38f692d8b8b5345c1e
maria <almendrasmaria04@gmail.com>
2026-01-18 19:55:29 -0300

    Add CatsGrid component to HomePage

--- client/src/features/home/pages/HomePage.tsx
+++ client/src/features/home/pages/HomePage.tsx
@@ -1,13 +1,25 @@
 import Navbar from "../../../shared/ui/Navbar";
 import HeroHome from "../components/HeroHome";
+import CatsGrid from "../../cats/components/CatsGrid";
+import { mockCats } from "../../cats/data/mockCats";
 
 const HomePage = () => {
-  return (
-    <div className="min-h-screen bg-white">
-      <Navbar />
-      <HeroHome />
-    </div>
-  );
+	return (
+		<div className="min-h-screen bg-[#F6F7F9]">
+			<Navbar />
+			<main>
+				<HeroHome />
+
+				<section className="py-12">
+					<div className="w-full px-5 md:px-[50px]">
+						<div className="mt-6">
+							<CatsGrid cats={mockCats} />
+						</div>
+					</div>
+				</section>
+			</main>
+		</div>
+	);
 };
 
 export default HomePage;
