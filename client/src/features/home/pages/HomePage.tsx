3575e2b8193302ed27ee7a2b94f935f9f7dbeb2a
maria <almendrasmaria04@gmail.com>
2026-01-19 03:19:22 -0300

    Refactor HomePage to integrate useCatSearch

--- client/src/features/home/pages/HomePage.tsx
+++ client/src/features/home/pages/HomePage.tsx
@@ -2,24 +2,28 @@ import Navbar from "../../../shared/ui/Navbar";
 import HeroHome from "../components/HeroHome";
 import CatsGrid from "../../cats/components/CatsGrid";
 import { mockCats } from "../../cats/data/mockCats";
+import { useCatSearch } from "../../cats/hooks/useCatFilters";
 
 const HomePage = () => {
-	return (
-		<div className="min-h-screen bg-[#F6F7F9]">
-			<Navbar />
-			<main>
-				<HeroHome />
+  const { query, setQuery, filteredCats } = useCatSearch(mockCats);
 
-				<section className="py-12">
-					<div className="w-full px-5 md:px-[50px]">
-						<div className="mt-6">
-							<CatsGrid cats={mockCats} />
-						</div>
-					</div>
-				</section>
-			</main>
-		</div>
-	);
+  return (
+    <div className="min-h-screen bg-[#F6F7F9]">
+      <Navbar />
+
+      <main>
+        <HeroHome query={query} onQueryChange={setQuery} />
+
+        <section className="py-12">
+          <div className="w-full px-4 md:px-[50px]">
+            <div className="mt-6">
+              <CatsGrid cats={filteredCats} />
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default HomePage;
