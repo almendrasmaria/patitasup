b4a46786030ee55ea7cd1ae31e24625e2b193885
maria <almendrasmaria04@gmail.com>
2026-01-20 15:38:35 -0300

    Add HowItWorks route

--- client/src/app/routes.tsx
+++ client/src/app/routes.tsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../features/home/pages/HomePage";
+import HowItWorksPage from "../features/how-it-works/pages/HowItWorksPage";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
+  { path: "/", element: <HomePage /> },
+  { path: "/como-funciona", element: <HowItWorksPage /> },
 ]);
