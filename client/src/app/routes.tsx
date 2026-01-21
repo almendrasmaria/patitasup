d1d502848b51dafaa06d94628245c856e80929e7
maria <almendrasmaria04@gmail.com>
2026-01-21 01:14:07 -0300

    Add contact route

--- client/src/app/routes.tsx
+++ client/src/app/routes.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../features/home/pages/HomePage";
 import HowItWorksPage from "../features/how-it-works/pages/HowItWorksPage";
+import ContactPage from "../features/contact/pages/ContactPage";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/como-funciona", element: <HowItWorksPage /> },
+  { path: "/contacto", element: <ContactPage /> },
 ]);
